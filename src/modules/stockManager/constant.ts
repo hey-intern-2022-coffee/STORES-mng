e8b54215eba26d4a275df7ad6ac9fc0172061057
Jabelic <hibiki990523@gmail.com>
2022-09-13 16:17:44 +0900

    FIX: style

---
+++ src/modules/stockManager/constant.ts
@@ -0,0 +1,1 @@
+export const APP_NAME = 'STORES在庫管理'
--- src/pages/stockManager/offline/index.vue
+++ src/pages/stockManager/offline/index.vue
@@ -1,5 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const handleClick = () => {
+  console.log('click')
+}
+
+const tableData = [
+  {
+    name: '商品名1',
+    price: 1000,
+    image_url: 'https://hogehuga.com',
+    stocks: 10
+  }
+]
+</script>
 <template>
   <div>offline stock manager</div>
+  <div>
+    <div class="table-wrapper">
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="name" label="商品名" />
+        <el-table-column prop="price" label="値段" />
+        <el-table-column prop="image_url" label="画像" />
+        <el-table-column prop="stocks" label="在庫数" />
+        <el-table-column fixed="right" label="Operations">
+          <template #default>
+            <el-button link type="primary" size="small" @click="handleClick"
+              >編集</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 <style scoped></style>
